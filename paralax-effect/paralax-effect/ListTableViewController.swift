fcaf086e56e339412ed9c4a12c0e5f0a2acd9f4e
Fragoso <hfragoso@MXN1800100-2.local>
2020-03-02 22:47:36 -0600

    initial commit

---
+++ paralax-effect/paralax-effect/ListTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  paralax-effect
+//
+//  Created by Fragoso, Hector on 3/2/20.
+//  Copyright © 2020 Fragoso, Hector. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func numberOfSections(in ListTableView: UITableView) -> Int {
+        return 1
+    }
+    
+    overrride func tableView(_ tableView:UITableView, numberOfRowsInSection section: Int) -> Int {
+        //Return number of cells in the only section of the tableview
+        return 5
+    }
+    
+    override func tableView(_ tableView:UITableView, cellForRowAt indexPath: indexPath) -> UITableViewCell {
+        // return a configured cell w/image and name
+    }
+}
+
